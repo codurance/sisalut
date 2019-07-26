33756cc6956b75614e15125e18cabd2f1e3f72b3
Gabriele Lippi <gabriele@lippi.net>
2019-07-26 15:21:51 +0200

    update build

---
+++ precache-manifest.98ca1abdbb6c76d311a52c17af6faff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf45d2d3fe45a0810f6f5990050d057",
+    "url": "/vx-playground/index.html"
+  },
+  {
+    "revision": "df3622e3ed0a8b4a6023",
+    "url": "/vx-playground/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "df3622e3ed0a8b4a6023",
+    "url": "/vx-playground/static/js/2.481cc272.chunk.js"
+  },
+  {
+    "revision": "420ed3a14da645392a90",
+    "url": "/vx-playground/static/js/main.9f81cede.chunk.js"
+  },
+  {
+    "revision": "34de6d2a45f550aeb6f6",
+    "url": "/vx-playground/static/js/runtime~main.cf0b68d8.js"
+  }
+]);
